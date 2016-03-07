12351ef1286c3a31cccca96bd2b204257c9546fc
Sargun Vohra <sargunster@gmail.com>
2016-03-07 02:10:04 -0500

    cleanup

--- src/main/kotlin/me/sargunvohra/lib/ktunits/time.kt
+++ src/main/kotlin/me/sargunvohra/lib/ktunits/time.kt
@@ -1,40 +1,28 @@
 package me.sargunvohra.lib.ktunits
 
-val Long.nanoseconds: TimeValue
-    get() = TimeValue(this)
+val Long.nanoseconds: TimeValue get() = TimeValue(this)
 
-val Long.milliseconds: TimeValue
-    get() = (this * 1000000).nanoseconds
+val Long.milliseconds: TimeValue get() = (this * 1000000).nanoseconds
 
-val Long.seconds: TimeValue
-    get() = (this * 1000).milliseconds
+val Long.seconds: TimeValue get() = (this * 1000).milliseconds
 
-val Long.minutes: TimeValue
-    get() = (this * 60).seconds
+val Long.minutes: TimeValue get() = (this * 60).seconds
 
-val Long.hours: TimeValue
-    get() = (this * 60).minutes
+val Long.hours: TimeValue get() = (this * 60).minutes
 
-val Long.days: TimeValue
-    get() = (this * 24).hours
+val Long.days: TimeValue get() = (this * 24).hours
 
-val Int.nanoseconds: TimeValue
-    get() = toLong().nanoseconds
+val Int.nanoseconds: TimeValue get() = toLong().nanoseconds
 
-val Int.milliseconds: TimeValue
-    get() = toLong().milliseconds
+val Int.milliseconds: TimeValue get() = toLong().milliseconds
 
-val Int.seconds: TimeValue
-    get() = toLong().seconds
+val Int.seconds: TimeValue get() = toLong().seconds
 
-val Int.minutes: TimeValue
-    get() = toLong().minutes
+val Int.minutes: TimeValue get() = toLong().minutes
 
-val Int.hours: TimeValue
-    get() = toLong().hours
+val Int.hours: TimeValue get() = toLong().hours
 
-val Int.days: TimeValue
-    get() = toLong().days
+val Int.days: TimeValue get() = toLong().days
 
 data class TimeValue internal constructor(internal val ns: Long) {
     val toNanoseconds = ns
