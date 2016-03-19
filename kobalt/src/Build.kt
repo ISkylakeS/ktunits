36151c98f583e7a985bde7803f6dde7a70dba9f5
Sargun Vohra <sargunster@gmail.com>
2016-03-18 22:53:30 -0400

    remove unnecessay stuff from build file

--- kobalt/src/Build.kt
+++ kobalt/src/Build.kt
@@ -2,23 +2,12 @@ import com.beust.kobalt.*
 import com.beust.kobalt.plugin.packaging.assemble
 import com.beust.kobalt.plugin.publish.bintray
 
-val repos = repos()
-
-
 val p = project {
 
     name = "KtUnits"
     group = "me.sargunvohra.lib"
     artifactId = name
     version = "1.1.0"
-
-    sourceDirectories {
-        path("src/main/kotlin")
-    }
-
-    sourceDirectoriesTest {
-        path("src/test/kotlin")
-    }
 
     dependencies {
     	compile("org.jetbrains.kotlin:kotlin-stdlib:1.0.0")
