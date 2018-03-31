9af348760963d321fb0705a7e36d656184d40d52
Alexander Shevchenko <34604839+ISkylakeS@users.noreply.github.com>
2018-04-01 00:05:54 +0300

    Update and rename time.kt to Time.kt

--- src/main/kotlin/me/sargunvohra/lib/ktunits/Time.kt
+++ src/main/kotlin/me/sargunvohra/lib/ktunits/Time.kt
@@ -1,31 +1,20 @@
+@file:JvmName("Time")
 package me.sargunvohra.lib.ktunits
 
 val Long.nanoseconds: TimeValue get() = TimeValue(this)
-
 val Long.microseconds: TimeValue get() = (this * 1000).nanoseconds
-
 val Long.milliseconds: TimeValue get() = (this * 1000).microseconds
-
 val Long.seconds: TimeValue get() = (this * 1000).milliseconds
-
 val Long.minutes: TimeValue get() = (this * 60).seconds
-
 val Long.hours: TimeValue get() = (this * 60).minutes
-
 val Long.days: TimeValue get() = (this * 24).hours
 
 val Int.nanoseconds: TimeValue get() = toLong().nanoseconds
-
 val Int.microseconds: TimeValue get() = toLong().microseconds
-
 val Int.milliseconds: TimeValue get() = toLong().milliseconds
-
 val Int.seconds: TimeValue get() = toLong().seconds
-
 val Int.minutes: TimeValue get() = toLong().minutes
-
 val Int.hours: TimeValue get() = toLong().hours
-
 val Int.days: TimeValue get() = toLong().days
 
 data class TimeValue internal constructor(internal val ns: Long) {
